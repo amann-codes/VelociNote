8f33c783f6e4ce85195e1e94eea323662e3aed6d
amanprajapati00 <aman.prajapati3000@gmail.com>
2024-10-06 22:54:33 +0530

    fixed bug

--- app/api/createUser/route.ts
+++ app/api/createUser/route.ts
@@ -1,21 +1,46 @@
 import prisma from "@/prisma/prisma";
 import { NextResponse } from "next/server";
-
+import zod from "zod";
+import bcrypt from 'bcrypt';
 export async function POST(req: Request) {
-    try {
+  try {
+    const userSchema = zod.object({
+      name: zod.string(),
+      email: zod.string().email(),
+      password: zod.string().min(6),
+    });
     const body = await req.json();
-    const { name, email, password } = body;
-    const result = await prisma.user.create({
-      data: {
-        name: name,
-        email: email,
-        password: password,
+    const { name, email, password } = userSchema.parse(body);
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email,
       },
     });
-    return NextResponse.json({
-      result,
-    });} catch (e) {
-      console.log(e);
+    if (existingUser) {
+      return NextResponse.json(
+        {
+          user: null,
+          message: "user already exists on this email",
+        },
+        { status: 409 }
+      );
     }
+    const passhash = await bcrypt.hash(password, 10);
+    const result = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: passhash,
+      },
+    });
+    console.log(result)
+    return NextResponse.json({
+      message:"user created"
+    })
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json({
+      error:e
+    })
   }
-
+}
