946ca1cb26e650224f5bad9d89c90da5cacb62d0
amanprajapati00 <aman.prajapati3000@gmail.com>
2024-10-06 22:55:14 +0530

    added back the providers to get session data

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,12 +1,10 @@
-// 'use client'
-// import React from 'react'
-// import { SessionProvider } from 'next-auth/react';
-// export default function Providers({children}:{children:React.ReactNode}) {
-//   return (
-//     <div>
-//         <SessionProvider>
-//             {children}
-//         </SessionProvider>
-//     </div>
-//   )
-// }
+"use client";
+import React from "react";
+import { SessionProvider } from "next-auth/react";
+export default function Provider({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <SessionProvider>{children}</SessionProvider>
+    </div>
+  );
+}
