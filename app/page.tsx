2f1b0a1046c7324e63cca3d601b58fe01a8b6c20
amanprajapati00 <aman.prajapati3000@gmail.com>
2024-10-08 23:38:10 +0530

    added session data to check authentication status

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import Appbar from '@/components/Appbar'
+"use client";
+import React from "react";
+import Appbar from "@/components/Appbar";
+import { useSession } from "next-auth/react";
 export default function page() {
+  const session = useSession();
+  const data = JSON.stringify(session);
+  console.log(session.data?.user?.name);
   return (
-    <div className='w-screen h-screen bg-lime-100'>
-      <Appbar/>
+    <div className="w-screen h-screen bg-lime-100">
+      <Appbar />
+      <h1>HIII</h1>
+      {data}{session.data?.user?.name}
+      <img src={session.data?.user?.image?.toString()} className="rounded-full"></img>
     </div>
-  )
+  );
 }
