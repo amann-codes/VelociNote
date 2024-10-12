ae4443472405b84806c7768315e69dd3c53c2af6
amanprajapati00 <aman.prajapati3000@gmail.com>
2024-10-12 22:18:15 +0530

    configured auth file

---
+++ lib/auth.config.ts
@@ -0,0 +1,88 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+import bcrypt from "bcrypt";
+import { NextAuthOptions } from "next-auth";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Username",
+          type: "text",
+          placeholder: "username",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Password",
+        },
+      },
+      authorize: async (credentials) => {
+        if (!credentials?.email || !credentials?.password) {
+          console.log("user not found");
+          return null;
+        }
+        const existingUser = await prisma.user.findFirst({
+          where: {
+            email: credentials?.email,
+          },
+          select: {
+            email: true,
+            password: true,
+            id: true,
+            name: true,
+          },
+        });
+        if (
+          existingUser &&
+          existingUser.password &&
+          (await bcrypt.compare(credentials.password, existingUser.password))
+        ) {
+          console.log("user found with correct credentials");
+          return {
+            id: existingUser.id.toString(),
+            email: existingUser.email,
+            name: existingUser.name,
+          };
+        }
+        console.log("user not found");
+        return null;
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID || "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+    }),
+  ],
+  callbacks: {
+    jwt: async ({ user, token }: any) => {
+      if (user) {
+        token.uid = user.id;
+      }
+      return token;
+    },
+    session: ({ session, token, user }: any) => {
+      if (session.user) {
+        session.user.id = token.uid;
+      }
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/signin",
+  },
+  session: {
+    strategy: "jwt",
+  },
+};
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,59 +1,10 @@
-import prisma from "@/prisma/prisma";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import CredentialsProvider from "next-auth/providers/credentials";
-import GoogleProvider from "next-auth/providers/google";
-export const authOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: {
-          label: "Username",
-          type: "text",
-          placeholder: "username",
-        },
-        password: {
-          label: "Password",
-          type: "password",
-          placeholder: "Password",
-        },
-      },
-      async authorize(credentials: any) {
-        const res = await prisma.user.findFirst({
-          where: {
-            email: credentials.username,
-          },
-          select: {
-            id:true,
-            password: true,
-            name: true,
-          },
-        });
-        if(res){
-          console.log("found")
-        }
-        return {
-          id:credentials.id,
-          name: credentials.name,
-          email: credentials.username,
-        };
-      },
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-    }),
-  ],
-  adapter: PrismaAdapter(prisma),
-  secret: process.env.NEXTAUTH_SECRET,
-  session: {
-    strategy: "jwt",
-  },
-  jwt: {
-    secret: process.env.NEXTAUTH_SECRET,
-    maxAge: 5 * 60 * 1000,
-  },
-  pages: {
-    signIn: "/signin",
-  },
-};
+import NextAuth from "next-auth";
+import { authOptions } from "./auth.config";
+
+export const {
+  handler: { GET, POST },
+  auth,
+} = NextAuth({
+  session: { strategy: "jwt" },
+  ...authOptions,
+});
