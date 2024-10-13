03cc6b217fdcd35c4d1ed0d82ba6c9ade976ac48
amanprajapati00 <aman.prajapati3000@gmail.com>
2024-10-13 21:35:01 +0530

    layout for dashboard

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,21 @@
+import SideBar from "@/components/SideBar";
+import { OrgSideBar } from "@/components/OrgSideBar";
+import NavBar from "@/components/NavBar";
+interface DashboardLayoutProps {
+  children: React.ReactNode;
+}
+const DashboardLayout = ({ children }: DashboardLayoutProps) => {
+  return (
+    <main className="h-full">
+      <SideBar />
+      <div className="pl-[60px] h-full">
+        <div className="flex gap-x-3 h-full">
+          <OrgSideBar />
+          <div className="h-full flex-1"><NavBar/>{children}</div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default DashboardLayout;
