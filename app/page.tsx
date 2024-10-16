c105d0d77616bacff5f5765c253f8ad9e1dc38bc
amanprajapati00 <aman.prajapati3000@gmail.com>
2024-10-16 23:20:46 +0530

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,6 @@ export default function page() {
   const data = JSON.stringify(session);
   console.log(session.data?.user?.name);
   return (
-    <div className="w-screen h-screen bg-[#F0F4F8]">
-      <Appbar />
-      <h1>Hi {session.data?.user?.name}</h1><br/>
-      {data}<br/>
-      <img src={session.data?.user?.image?.toString()} width={40} height={40} className="rounded-full"></img>
-    </div>
+    <p>{data}</p>
   );
 }
