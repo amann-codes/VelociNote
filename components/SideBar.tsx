a0769c33015042f119ce2678e017233cb94b6170
amanprajapati00 <aman.prajapati3000@gmail.com>
2024-10-17 22:50:47 +0530

    added organization listcomponent

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -2,11 +2,13 @@
 import React, { useState } from 'react'
 import AddOrg from './AddOrg'
 import Stepper from './Stepper';
+import OrganizationBox from './OrganizationBox';
 
 export default function SideBar() {
   const [open, setOpen] = useState(false);
   return (
-    <aside className='flex justify-center fixed z-[1] left-0 h-full gap-y-4 p-3 bg-blue-300'>
+    <aside className='flex flex-col justify-center fixed z-[1] left-0 h-full gap-y-4 p-3 bg-blue-300'>
+      <OrganizationBox/>
       <AddOrg open={()=>setOpen(true)}/>
       {open&&
       <Stepper close={()=>setOpen(false)}/>
