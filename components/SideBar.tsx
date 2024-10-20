0943b176ad27d1f0398031a5538bb8992a9dec09
amanprajapati00 <aman.prajapati3000@gmail.com>
2024-10-20 08:47:46 +0530

    Moved files to keep it organized

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,14 +1,12 @@
 "use client";
 import React, { useState } from 'react'
-import AddOrg from './AddOrg'
-import Stepper from './Stepper';
-import OrganizationBox from './OrganizationBox';
+import AddOrg from './Organization/AddOrg';
+import Stepper from './Organization/Stepper';
 
 export default function SideBar() {
   const [open, setOpen] = useState(false);
   return (
     <aside className='flex flex-col justify-center fixed z-[1] left-0 h-full gap-y-4 p-3 bg-blue-300'>
-      <OrganizationBox/>
       <AddOrg open={()=>setOpen(true)}/>
       {open&&
       <Stepper close={()=>setOpen(false)}/>
