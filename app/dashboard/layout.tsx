032a7919ba76edea4275d4a3b0e458ac30bebf6f
amanprajapati00 <aman.prajapati3000@gmail.com>
2024-10-21 23:56:41 +0530

    changed layout for dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import SideBar from "@/components/SideBar";
+import SideBar from "@/components/Organization/SideBar";
 import { OrgSideBar } from "@/components/Organization/OrgSideBar";
 import NavBar from "@/components/NavBar";
 interface DashboardLayoutProps {
@@ -8,14 +8,24 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   return (
     <main className="h-full">
       <SideBar />
-      <div className="pl-[60px] h-full">
-        <div className="flex gap-x-3 h-full">
-          <OrgSideBar />
-          <div className="h-full flex-1"><NavBar/>{children}</div>
+      <div className="flex pl-16">
+        <OrgSideBar />
+        <div className="w-full">
+          <NavBar />
+          {children}
         </div>
       </div>
     </main>
   );
 };
+
+{
+  /* <div className="pl-[60px] h-full">
+        <div className="flex gap-x-3 h-full">
+          <OrgSideBar />
+          <div className="h-full flex-1"><NavBar/>{children}</div>
+        </div>
+      </div> */
+}
 
 export default DashboardLayout;
