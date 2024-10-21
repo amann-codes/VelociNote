1023578cc4cfe6ac56c2b903d38fa0bd0d0a1f7a
amanprajapati00 <aman.prajapati3000@gmail.com>
2024-10-21 23:56:20 +0530

    Select organization option

--- components/Organization/OrgSideBar.tsx
+++ components/Organization/OrgSideBar.tsx
@@ -1,9 +1,46 @@
 "use client";
+import { useState, useEffect } from "react";
+import Icon from "../Icon";
+
+interface Organization {
+  id: string;
+  name: string;
+}
 
 export const OrgSideBar = () => {
+  const [organizations, setOrganizations] = useState<Organization[]>([]);
+  const [orgName, setOrgName] = useState("")
+  useEffect(() => {
+    const fetchOrganizations = async () => {
+      try {
+        const res = await fetch("/api/organization", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const data = await res.json();
+        setOrganizations(data.orgs);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchOrganizations();
+  }, []);
   return (
-    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 bg-red-500">
-      Org Sidebar
+    <div className="w-[200px] mx-2 mt-4">
+      <span className="text-2xl font-serif bg-gray-300 rounded-md border-2 border-gray-500 p-2">
+        VelociNote
+      </span>
+      <select className="mt-8">
+        {organizations.map((items, index) => {
+          return <option key={index} value={items.id} onClick={()=>setOrgName(items.id)}>{items.name}</option>;
+        })}
+      </select>
+      <div className="text-red-700 font-bold">
+        org
+      </div>
     </div>
   );
 };
